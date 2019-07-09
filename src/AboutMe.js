d32523a29d025f5e82497ec221e31b2ea3c36f37
yuchin <yuchinpao@liontravel.com>
2019-07-09 23:27:40 +0800

    fnish about me

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -8,21 +8,19 @@ const AboutMe = () => {
       <h1 className="color_2 color_3 title_space">About Me</h1>
       <Grid xs={12} sm={12} md={12}>
         <div className="aboutMe_content color_3">
-I was majored in Department of english literature in university and working as a Front end engineer for more than one year.
-When people say "how could you jump to Front-end engineer?", I would say this is not a big gap when you have PASSION.
+          Hi! I am Yu Chin. I graduated with a Bachelor of Foreign Language and has been working as a front-end engineer for
+          the past year.           
+          When people ask me why would you make such a leap, I would tell them that the gap is made small when you have PASSION. <br />
 
-
-I take advantage of my language expertise to get knowledge of my front-end 
-
-
-For improve myself, I also learning german as my third language and successfully applied exchanged student in Germany for one year. 
-
-I 'm not only technical but I also have hand-on experience with Google Ads, Google analysis, and marketing.
-
-Learning new things is excited for me. I really like the creativity and problem-solving that comes with it.
-Hundreds of JavaScript frameworks popped up. Using frameworks is currently my XXXX.
-
-My front-end journey started from web and JavaScript. Software engineering already occupied an important place in modern technologies. I'm glad I'm in.
+          To improve myself, I also learned German and successfully applied exchanged student in Germany for one year. 
+          Because of this opportunity, I step into field of Front-end development.<br />
+          
+          I am not only technical but I also have hand-on experience with Google Ads, Google analysis, and marketing.
+          
+          Learning something new gets me access to new and different opportunities. 
+          I am really into the problem-solving and the creativeness that come along it. 
+          
+          Software engineering already occupied an important place in modern technologies. I'm glad I'm in.
         </div>
       </Grid>
     </div>

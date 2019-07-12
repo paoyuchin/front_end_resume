f919da3c7715d70dbb46c82cf08d20f198094cde
yuchin <yuchinpao@liontravel.com>
2019-07-12 13:47:00 +0800

    add project

--- src/Project.js
+++ src/Project.js
@@ -42,7 +42,7 @@ const Project = () => {
   return (
     <div className="Project">
       <h1 className="color_2 color_3 title_space">Projects & developments</h1>
-      <VerticalTimeline layout={'1-column'}>
+      <VerticalTimeline layout={"1-column"}>
         <VerticalTimelineElement
           className="lion_work"
           iconStyle={{ background: "#0073b0" }}
@@ -56,8 +56,10 @@ const Project = () => {
         >
           <div className={classes.title}>
             <div>
-              <span className="skillsButton">adddda</span>
-              <span className="skillsButton">bb</span>
+              <span className="skillsButton">React.js</span>
+              <span className="skillsButton">Jquery</span>
+              <span className="skillsButton">RWD</span>
+              <span className="skillsButton">webpack</span>
             </div>
             <h3 className="vertical-timeline-element-title">
               Lion Travel - Ticket
@@ -65,12 +67,17 @@ const Project = () => {
             <p
               className={`vertical-timeline-element-subtitle ${classes.name}`}
             >
-              content content content content content content content content
-              content content content
+              <li>In charge of Lion Travel's Ticket website</li>
+              The most challenge part in the project is that projec built with
+              Jquery and React.js. The search panel built with react. And
+              other parts build with Jquery. Because of the differenct
+              environmentm. I need to pack with two differenct bundle file to
+              intergret with. Also needs to discuss with back-end engineer
+              about data connection.
             </p>
 
             <a className="button" href="">
-              go
+              Visit website
             </a>
           </div>
         </VerticalTimelineElement>
@@ -81,27 +88,36 @@ const Project = () => {
             <i
               className={`fas fa-suitcase iconMediaquery ${
                 classes.iconstyle
-                }`}
+              }`}
             />
           }
         >
           <div className={classes.title}>
             <div>
-              <span className="skillsButton">adddda</span>
-              <span className="skillsButton">bb</span>
+              <span>Ract</span>
+              <span>Ract-Redux</span>
+              <span>Rect-Router</span>
+              <span className="skillsButton">Firebase</span>
+              <span className="skillsButton">Firestore</span>
+              <span className="skillsButton">
+                firebase google authentication
+              </span>
+              <span className="skillsButton">RWD</span>
             </div>
             <h3 className="vertical-timeline-element-title">
-              Lion Travel - Ticket
+              年輕人最喜歡的-穀米養生粥
             </h3>
             <p
               className={`vertical-timeline-element-subtitle ${classes.name}`}
             >
-              content content content content content content content content
-              content content content
+              I am making a website for my best Friend without any payment
+              from scratch by myself currently. Cloud Firestore is flexible,
+              scalable database for web. For beginner learning database, which
+              is a good startup.
             </p>
 
             <a className="button" href="">
-              go
+              Visit website
             </a>
           </div>
         </VerticalTimelineElement>
@@ -112,31 +128,93 @@ const Project = () => {
             <i
               className={`fas fa-suitcase iconMediaquery ${
                 classes.iconstyle
-                }`}
+              }`}
             />
           }
         >
           <div className={classes.title}>
             <div>
-              <span className="skillsButton">adddda</span>
-              <span className="skillsButton">bb</span>
+              <span>Ract</span>
+              <span>RWD</span>
             </div>
             <h3 className="vertical-timeline-element-title">
-              Lion Travel - Ticket
+              React Anchor Module
             </h3>
             <p
               className={`vertical-timeline-element-subtitle ${classes.name}`}
             >
-              content content content content content content content content
-              content content content
+              I made a react module, which using in lion travel's index.
             </p>
 
             <a className="button" href="">
-              go
+              Visit website
             </a>
           </div>
         </VerticalTimelineElement>
-        
+        <VerticalTimelineElement
+          className="lion_work"
+          iconStyle={{ background: "#0073b0" }}
+          icon={
+            <i
+              className={`fas fa-suitcase iconMediaquery ${
+                classes.iconstyle
+              }`}
+            />
+          }
+        >
+          <div className={classes.title}>
+            <div>
+              <span className="skillsButton">php</span>
+              <span className="skillsButton">mySql</span>
+              <span className="skillsButton">javascript</span>
+              <span>scss</span>
+            </div>
+            <h3 className="vertical-timeline-element-title">Pure css, svg</h3>
+            <p
+              className={`vertical-timeline-element-subtitle ${classes.name}`}
+            >
+              This is my first website with php and mySql when I was learning
+              front-end skills in Institute for Information Industry.
+            </p>
+
+            <a className="button" href="">
+              Visit website
+            </a>
+          </div>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="lion_work"
+          iconStyle={{ background: "#0073b0" }}
+          icon={
+            <i
+              className={`fas fa-suitcase iconMediaquery ${
+                classes.iconstyle
+              }`}
+            />
+          }
+        >
+          <div className={classes.title}>
+            <div>
+              <span className="skillsButton">scss</span>
+              <span className="skillsButton">svg</span>
+              <span className="skillsButton">Pug</span>
+              <span>scss</span>
+            </div>
+            <h3 className="vertical-timeline-element-title">
+              Pure css with some stuffs
+            </h3>
+            <p
+              className={`vertical-timeline-element-subtitle ${classes.name}`}
+            >
+              This is my first website built with php and mySql when I was
+              learning front-end skills in Institute for Information Industry.
+            </p>
+
+            <a className="button" href="">
+              Visit website
+            </a>
+          </div>
+        </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
   );

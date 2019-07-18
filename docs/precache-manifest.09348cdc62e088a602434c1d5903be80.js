89e4bdd01eb444bd42c47da6da6e3d9f42c499e6
yuchin <yuchinpao@liontravel.com>
2019-07-18 13:09:44 +0800

    add socs

---
+++ docs/precache-manifest.09348cdc62e088a602434c1d5903be80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28213f648f8014253e2fc3fe7b4090f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66f48b53ec59b4b363ed",
+    "url": "/static/css/2.38750c1c.chunk.css"
+  },
+  {
+    "revision": "193b104ca86cccec97bc",
+    "url": "/static/css/main.cd039abc.chunk.css"
+  },
+  {
+    "revision": "66f48b53ec59b4b363ed",
+    "url": "/static/js/2.76bccd5f.chunk.js"
+  },
+  {
+    "revision": "193b104ca86cccec97bc",
+    "url": "/static/js/main.5513df94.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
